5b14e8c3bad91ea35dfbe62ebdb0d60cafdab58d
Fabian <fabianchock@gmail.com>
2022-11-12 18:38:56 -0300

    commit de prueba de gitignore

--- PruebasMonoBbiour/Assets/Sripts/MouseEvents.cs
+++ PruebasMonoBbiour/Assets/Sripts/MouseEvents.cs
@@ -5,15 +5,17 @@ using UnityEngine;
 public class MouseEvents : MonoBehaviour{
 
     Renderer color;
-    private void OnMouseEnter()
-    {
-        color = GetComponent<Renderer>();
-        color.material.color = Color.red;
+     private void OnMouseEnter()
+     {
+         color = GetComponent<Renderer>();
+         color.material.color = Color.red;
+        print("el mouse ha entrado en contacto con el objeto");
 
-    }
+     }
     private void OnMouseOver()
     {
         color.material.color = Color.blue;
+        print("el mouse esta en contacto con el objeto ahora");
     }
 
     private void OnMouseExit()
