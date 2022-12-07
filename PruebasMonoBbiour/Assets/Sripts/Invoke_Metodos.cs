2f82c7bd3b16746ea6951add03075bc45ad206f7
Fabian <fabianchock@gmail.com>
2022-12-06 21:46:21 -0300

    Metodos invoke practica

--- PruebasMonoBbiour/Assets/Sripts/Invoke_Metodos.cs
+++ PruebasMonoBbiour/Assets/Sripts/Invoke_Metodos.cs
@@ -7,12 +7,23 @@ public class Invoke_Metodos : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Invoke("EscribirHola", 0.2f);
+        /*el metodo invoke invoca un metodo que cancela las invocaciones
+         El metodo invoke repeating invoca un metodo que escribe primero con un tiempo de retraso de 1 y luego lo hace cada 5 segs*/
+        
+        InvokeRepeating("EscribirHola", 1.0f, 5.0f);
+        //cancela las invocaciones y lo anuncia x texto
+        Invoke("CancelarInvocaciones", 10.0f);
     }
 
     // Este metodo te saluda x consola
     void EscribirHola()
     {
-        Debug.Log("HOLAAAAAAAAAAAAAAAAAAAA");
+        print("HOLAAAAAAAAAAAAA Holanda");
+    }
+    //se invoca un metodo que finaliza las invocaciones y se anuncia x texto 
+    void CancelarInvocaciones()
+    {
+        CancelInvoke();
+        print("invocaciones finalizadas");
     }
 }
